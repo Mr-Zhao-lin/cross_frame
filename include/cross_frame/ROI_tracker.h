e0e3ed87c318bb688cd9c9bdd633dfc35dd8a491
zdl <2327128313@qq.com>
2022-10-31 06:03:18 -0700

    能跟踪了,现在进行性能优化,去掉检测第二大的框

--- include/cross_frame/ROI_tracker.h
+++ include/cross_frame/ROI_tracker.h
@@ -12,6 +12,9 @@
 using namespace cv;
 using namespace std;
 
+void findSquares(const cv::Mat& image,vector<vector<Point> >& squares,cv::Mat &out);
+
+template <class  Tracker_type>
 class Tracker_ROI
 {
     /*
@@ -23,28 +26,178 @@ class Tracker_ROI
 public:
     cv::Rect_<int> bbox;
     cv::Mat frame;
-    cv::Ptr<cv::Tracker> tracker;
-    Ptr<legacy::Tracker>  tracker_legacy;
+    Tracker_type tracker;
+    //cv::Ptr<cv::Tracker> tracker;
+    //Ptr<legacy::Tracker>  tracker_legacy;
     bool track_status;
     Tracker_ROI(cv::Mat& _frame,const std::string &_trackerTypes="KCF");//初始化tracker,用鼠标框选
     Tracker_ROI(const cv::Mat &_frame,const std::string &_trackerTypes="KCF",const cv::Rect_<int> bbox={287, 23, 86, 320});//初始化tracker
     void New_frame(cv::Mat& _frame);
 };
 
-class Object_detector
+
+template <>
+class Tracker_ROI<Ptr<legacy::Tracker>>   ///类的显世具体化，针对legacy中的tracker
 {
     /*
-    合作目标的检测与分离
-    */
-
+     * 类方法：
+     * 初始化跟踪器：tracker种类,初始frame，ROI选择
+     * frame的输入
+     * ROI的选择
+     * 读取下一frame并追踪，返回bbox位置，size*/
+public:
+    cv::Rect_<double> bbox;
+    cv::Mat frame;
+    Ptr<legacy::Tracker> tracker;
+    //cv::Ptr<cv::Tracker> tracker;
+    //Ptr<legacy::Tracker>  tracker_legacy;
+    bool track_status;
+    Tracker_ROI(cv::Mat& _frame,const std::string &_trackerTypes="KCF");//初始化tracker,用鼠标框选
+    Tracker_ROI(const cv::Mat &_frame,const std::string &_trackerTypes="KCF",const cv::Rect_<int> bbox={287, 23, 86, 320});//初始化tracker
+    void New_frame(cv::Mat& _frame);
 };
 
-
-void select_color()
-{
-
-};
-
-void findSquares(const cv::Mat& image,vector<vector<Point> >& squares,cv::Mat &out);
 
 #endif //ROI_TRACKER_ROI_TRACKER_H
+
+
+/////////////////////////////模板类声明和实现在同一文件
+template <class  Tracker_type>
+Tracker_ROI<Tracker_type>::Tracker_ROI(cv::Mat& _frame,const std::string &_trackerTypes) {
+    frame=_frame;
+
+    if (_trackerTypes == "MIL"){
+        tracker = TrackerMIL::create();
+    }
+    else if(_trackerTypes == "KCF"){
+        tracker = TrackerKCF::create();
+    }
+    else if (_trackerTypes == "GOTURN"){
+        tracker = TrackerGOTURN::create();
+    }
+    else if (_trackerTypes == "CSRT"){
+        tracker = TrackerCSRT::create();
+    }
+    else if (_trackerTypes == "MOSSE"){
+        //tracker = TrackerCSRT::create();
+       //legacy::TrackerMOSSE tracker_mosse;
+        //tracker=tracker_mosse.create();
+        //tracker=cv::legacy::TrackerMOSSE::create();
+    }
+    else{
+        std::cout<<"ERROR:No such tracker";
+    }
+    bbox = selectROI(frame, true);
+    rectangle(frame, bbox, Scalar( 255, 0, 0 ), 2, 1 );
+    imshow("Tracking", frame);
+    tracker->init(frame,bbox);
+
+}
+template <class  Tracker_type>
+Tracker_ROI<Tracker_type>::Tracker_ROI(const cv::Mat &_frame,const std::string &_trackerTypes,const cv::Rect_<int> bbox):track_status(1) {
+    frame=_frame;
+
+    if (_trackerTypes == "MIL"){
+        tracker = TrackerMIL::create();
+    }
+    else if(_trackerTypes == "KCF"){
+        tracker = TrackerKCF::create();
+    }
+    else if (_trackerTypes == "GOTURN"){
+        tracker = TrackerGOTURN::create();
+    }
+    else if (_trackerTypes == "CSRT"){
+        tracker = TrackerCSRT::create();
+    }
+    else if (_trackerTypes == "MOSSE"){
+        //tracker = TrackerCSRT::create();
+       //legacy::TrackerMOSSE tracker_mosse;
+        //tracker=tracker_mosse.create();
+        //tracker=cv::legacy::TrackerMOSSE::create();
+    }
+    else{
+        std::cout<<"ERROR:No such tracker";
+    }
+    rectangle(frame, bbox, Scalar( 255, 0, 0 ), 2, 1 );
+    imshow("Tracking", frame);
+    tracker->init(frame,bbox);
+
+}
+template <class  Tracker_type>
+void Tracker_ROI<Tracker_type>::New_frame(cv::Mat &_frame) {
+    frame=_frame;
+    track_status = tracker->update(frame, bbox);
+    if ( track_status)
+    {
+        // Tracking success : Draw the tracked object
+        rectangle(frame, bbox, Scalar( 255, 0, 0 ), 2, 1 );
+    }
+    else
+    {
+        // Tracking failure detected.
+        putText(frame, "Tracking failure detected", Point(100,80), FONT_HERSHEY_SIMPLEX, 0.75, Scalar(0,0,255),2);
+    }
+    imshow("Tracking", frame);
+    int k = waitKey(1);
+    if(k == 27)
+    {
+        std::exit(1);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Tracker_ROI<Ptr<legacy::Tracker>>::Tracker_ROI(cv::Mat& _frame,const std::string &_trackerTypes) {
+    frame=_frame;
+
+    tracker=cv::legacy::TrackerMOSSE::create();
+
+    bbox = selectROI(frame, true);
+    rectangle(frame, bbox, Scalar( 255, 0, 0 ), 2, 1 );
+    imshow("Tracking", frame);
+    tracker->init(frame,bbox);
+
+}
+
+Tracker_ROI<Ptr<legacy::Tracker>>::Tracker_ROI(const cv::Mat &_frame,const std::string &_trackerTypes,const cv::Rect_<int> bbox):track_status(1) {
+    frame=_frame;
+    tracker=cv::legacy::TrackerMOSSE::create();
+    rectangle(frame, bbox, Scalar( 255, 0, 0 ), 2, 1 );
+    imshow("Tracking", frame);
+    tracker->init(frame,bbox);
+
+}
+
+
+
+void Tracker_ROI<Ptr<legacy::Tracker>>::New_frame(cv::Mat &_frame) {
+    frame=_frame;
+    track_status = tracker->update(frame, bbox);
+    if ( track_status)
+    {
+        // Tracking success : Draw the tracked object
+        rectangle(frame, bbox, Scalar( 255, 0, 0 ), 2, 1 );
+    }
+    else
+    {
+        // Tracking failure detected.
+        putText(frame, "Tracking failure detected", Point(100,80), FONT_HERSHEY_SIMPLEX, 0.75, Scalar(0,0,255),2);
+    }
+    imshow("Tracking", frame);
+    int k = waitKey(1);
+    if(k == 27)
+    {
+        std::exit(1);
+    }
+}
--- src/main_run.cpp
+++ src/main_run.cpp
@@ -9,6 +9,13 @@
 #include"time.h"
 using namespace cv;
 using namespace std;
+#define i_type 4             //设置跟踪器类型
+    
+#if(i_type>=4)
+typedef Ptr<legacy::Tracker> tracker_type; //legacy tracker
+#else
+typedef Ptr<cv::Tracker>  tracker_type;
+#endif
 void dectect_square(VideoCapture &video,vector<vector<Point> > &squares);
 void track_square(VideoCapture &video,vector<vector<Point> > &squares);
 
@@ -20,7 +27,7 @@ int main(int argc, char **argv)
     //Tracker_ROI track_roi(frame,trackerType);//初始化
 
     VideoCapture capture;
-	capture.open("/home/zdl/catkin_ws/src/cross_frame/src/video1.webm");
+	capture.open("/home/zdl/catkin_ws/src/cross_frame/src/2022-10-31-005320.webm");
 	if (!capture.isOpened()) {
 		printf("could not find video");
 		return -1;
@@ -33,11 +40,16 @@ int main(int argc, char **argv)
     clock_t start,finish;
     start = clock();//初始化
 
-
+    int i=0;
     while(1)
     {
         // 检测框
-
+        /*while (capture.read(frame))
+        {
+            imwrite("/home/zdl/catkin_ws/src/cross_frame/src/img/"+to_string(i)+".png",frame);
+            i++;
+        }
+        */
         dectect_square(capture,squares);
         
         //  跟踪框    
@@ -64,13 +76,15 @@ void dectect_square(VideoCapture &video,vector<vector<Point> > &squares)
         frame_num++;
         imshow("input_video", frame);
         cv::cvtColor(frame, frame_hsv,cv::COLOR_BGR2HSV);
-		inRange(frame_hsv,Scalar(0.46*180,0.36*255,0.0*255),Scalar(0.6*180,0.7*255,1.0*255),mask);//过滤
-        //imshow("frame_hsv",mask);
+        inRange(frame_hsv,Scalar((324.0/360.0)*180,0.470*255,0.180*255),Scalar((350.0/360.0)*180,0.78*255,0.625*255),mask);//过滤
+		//inRange(frame_hsv,Scalar(0.46*180,0.36*255,0.0*255),Scalar(0.6*180,0.7*255,1.0*255),mask);//过滤
+        imshow("frame_hsv",mask);
        squares_picture=Mat::ones(mask.size(),mask.type())*255;
         findSquares( mask, squares,squares_picture);
-        if(squares.size()==1)
+        waitKey(100);
+        if(squares.size()>=1)
         {
-            waitKey(10);
+            waitKey(100);
             break;
         }
         
@@ -83,18 +97,18 @@ void track_square(VideoCapture &video,vector<vector<Point> > &squares)
     //cout<<"tracking"<<endl;
 
     string trackerTypes[5] = {"MIL", "KCF",  "GOTURN",  "CSRT", "MOSSE"};
-    string trackerType = trackerTypes[4];
+    string trackerType = trackerTypes[i_type];
     Mat frame;
     video.read(frame);
     int track_area=100;
     Rect_<int> bbox1(squares[0][0].x-track_area/2,squares[0][0].y-track_area/2,track_area,track_area);
-    Tracker_ROI track_roi_1(frame,trackerType,bbox1);//初始化
+    Tracker_ROI<tracker_type> track_roi_1(frame,trackerType,bbox1);//初始化
     Rect_<int> bbox2(squares[0][1].x-track_area/2,squares[0][1].y-track_area/2,track_area,track_area);
-    Tracker_ROI track_roi_2(frame,trackerType,bbox2);//初始化
+    Tracker_ROI< tracker_type> track_roi_2(frame,trackerType,bbox2);//初始化
     Rect_<int> bbox3(squares[0][2].x-track_area/2,squares[0][2].y-track_area/2,track_area,track_area);
-    Tracker_ROI track_roi_3(frame,trackerType,bbox3);//初始化
+    Tracker_ROI< tracker_type> track_roi_3(frame,trackerType,bbox3);//初始化
     Rect_<int> bbox4(squares[0][3].x-track_area/2,squares[0][3].y-track_area/2,track_area,track_area);
-    Tracker_ROI track_roi_4(frame,trackerType,bbox4);//初始化
+    Tracker_ROI< tracker_type> track_roi_4(frame,trackerType,bbox4);//初始化
     
     while (video.read(frame))
     {
